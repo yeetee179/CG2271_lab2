0dd454059f1ed391c9ad95c9c3dfb2968209fa7b
yeetee179 <yeetee179@gmail.com>
2017-03-30 20:57:36 +0800

    lab5qn5

--- lab5qn5/test.cpp
+++ lab5qn5/test.cpp
@@ -7,17 +7,28 @@
 
 #define STACK_SIZE 200
 
-xSemaphoreHandle semaphore0;
-xSemaphoreHandle semaphore1;
+xSemaphoreHandle semaphoreBinary = xSemaphoreCreateBinary();
+xSemaphoreHandle semaphoreCounting = xSemaphoreCreateCounting( (UBaseType_t)4,(UBaseType_t)  4 );
+xSemaphoreHandle semaphoreMutex = xSemaphoreCreateMutex();
 
-void int0task(void *p){
+int buf[4];
+int in=0, out = 0;
+
+void Producer(void *p){
 	while(1){
-		if(xSemaphoreTake( semaphore0, ( TickType_t ) 0)){
-			for(int i=0; i<5; i++){
-				digitalWrite(7, HIGH); // Set digital I/O pin to a 1
-				delay(125); // Delay
-				digitalWrite(7, LOW); // Set digital I/O pin to a 0
-				delay(125);
+		if(xSemaphoreTake( semaphoreBinary,(TickType_t) 0)){
+			if (xSemaphoreTake( semaphoreCounting,(TickType_t) 0)){
+				if(xSemaphoreTake( semaphoreMutex,(TickType_t) 0)){
+					buf[in] = analogRead(0);
+					in = (in + 1) % 4;
+					xSemaphoreGive( semaphoreMutex);
+				}
+				else {
+					xSemaphoreGive( semaphoreCounting);
+				}
+			}
+			else {
+				xSemaphoreGive(semaphoreBinary);
 			}
 		}
 		else{
@@ -26,55 +37,41 @@ void int0task(void *p){
 	}
 }
 
-// Flashes LED at pin 6: 5 times at 2HZ
-void int1task(void * p){
+void Consumer(void *p){
+	TickType_t currentTime = xTaskGetTickCount();
 	while (1){
-		if( xSemaphoreTake( semaphore1, ( TickType_t ) 0)){
-			for(int i=0; i<5; i++){
-				digitalWrite(6, HIGH); // Set digital I/O pin to a 1
-				delay(250); // Delay
-				digitalWrite(6, LOW); // Set digital I/O pin to a 0
-				delay(250);
+		if(xSemaphoreGive(semaphoreCounting)){
+			if (xSemaphoreTake( semaphoreMutex, (TickType_t) 0)){
+				Serial.println(buf[out]);
+				out = (out + 1) % 4;
+				xSemaphoreGive( semaphoreMutex);
+			}
+			else{
+				xSemaphoreTake(semaphoreCounting,(TickType_t) 0);
 			}
 		}
-		else{
-			vTaskDelay(1);
-		}
+		vTaskDelayUntil(&currentTime, 5000);
 	}
 }
 
-void int0ISR(){
+
+
+void ProducerISR(){
 	unsigned long current_interrupt_time = millis();
 	static unsigned long previous_Interrupt_time = 0;
 	if((current_interrupt_time - previous_Interrupt_time) > 200){
-		xSemaphoreGiveFromISR(semaphore0, NULL);
-	}
-	previous_Interrupt_time = current_interrupt_time;
-}
-
-
-void int1ISR(){
-	static BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-	unsigned long current_interrupt_time = millis();
-	static unsigned long previous_Interrupt_time = 0;
-	if((current_interrupt_time - previous_Interrupt_time) > 200){
-		xSemaphoreGiveFromISR(semaphore1, &xHigherPriorityTaskWoken);
+		xSemaphoreGiveFromISR(semaphoreBinary, NULL);
 	}
 	previous_Interrupt_time = current_interrupt_time;
 }
 
 void setup() {
-	attachInterrupt(digitalPinToInterrupt(3), int0ISR, FALLING);
-	attachInterrupt(digitalPinToInterrupt(2), int1ISR, FALLING);
-	pinMode(7, OUTPUT);
-	pinMode(6, OUTPUT);
-	semaphore0 = xSemaphoreCreateBinary();
-	semaphore1 = xSemaphoreCreateBinary();
+	Serial.begin(115200);
+	attachInterrupt(digitalPinToInterrupt(3), ProducerISR, FALLING);
 }
 
 void loop() {
-	xTaskCreate(int1task, "Task1", STACK_SIZE, NULL, 1, NULL);
-	xTaskCreate(int0task, "Task2", STACK_SIZE, NULL, 2, NULL);
+	xTaskCreate(Producer, "Producer", STACK_SIZE, NULL, 1, NULL);
+	xTaskCreate(Consumer, "Consumer", STACK_SIZE, NULL, 1, NULL);
 	vTaskStartScheduler();
 }
-
