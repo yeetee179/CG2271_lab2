3ebf1d5b6bd000e84d33735500843dccc230476b
yeetee179 <yeetee179@gmail.com>
2017-04-19 11:21:55 +0800

    .

--- lab6/test.cpp
+++ lab6/test.cpp
@@ -12,52 +12,57 @@ int desiredSpeed = 0;
 int currentSpeed = 0;
 int potentioValue = 0;
 int distance = 0;
-int redLedFlag = 0; //0 is no brake, 1 is brake
-QueueHandle_t currentSpeedQueue = xQueueCreate(2, sizeof(int));
-QueueHandle_t distanceQueue = xQueueCreate(2, sizeof(int));
+int redLedFlag = 0; //0 indicates no brake, 1 indicates brake
+QueueHandle_t currentSpeedQueue = xQueueCreate(2, sizeof(int)); // queue for current speed
+QueueHandle_t distanceQueue = xQueueCreate(2, sizeof(int));     //queue for distance value
 
+//lights up the yellow LED given the current speed
+//lights up the red LED if emergency braking is activated
+//buzzes the buzzer according to the current speed value
 void CurrentSpeedTask(void *p) {
-
 	while(1){
 		TickType_t redLedTime;
 		if (currentSpeed == 0 ){
 			digitalWrite(6, LOW);
 			digitalWrite(7, LOW);
 			digitalWrite(8, LOW);
-			analogWrite(5,0);
+			analogWrite(5,0);//buzzer is not buzzing
 		}
 		if (currentSpeed == 1 ){
 			digitalWrite(6, HIGH);
 			digitalWrite(7, LOW);
 			digitalWrite(8, LOW);
-			analogWrite(5, 5);
+			analogWrite(5, 5);//buzzer buzzes at volume 1
 		}
 		else if (currentSpeed == 2 ){
 			digitalWrite(6, HIGH);
 			digitalWrite(7, HIGH);
 			digitalWrite(8, LOW);
-			analogWrite(5,20);
+			analogWrite(5,20);//buzzer buzzes at volume 2
 		}
 		else if (currentSpeed == 3){
 			digitalWrite(6, HIGH);
 			digitalWrite(7, HIGH);
 			digitalWrite(8, HIGH);
-			analogWrite(5, 55);
+			analogWrite(5, 55);// buzzer buzzes at volume 3
 		}
 		if (redLedFlag == 1){
 			redLedTime = xTaskGetTickCount();
 			digitalWrite(9, HIGH);
 			vTaskDelayUntil(&redLedTime, 1000);
 			digitalWrite(9, LOW);
-			redLedFlag = 0;
+			redLedFlag = 0;// buzzer buzzes at volume 4
 		}
 	}
 }
 
+//reads in the potentiometer value
 void PotentiometerReadingTask(void *p) {
 	while(1){
 		TickType_t currentTime = xTaskGetTickCount();
 		potentioValue = analogRead(0);
+
+		//maps the potentiometer value  1 - 1023 to 4 distance values 1 - 4
 		if (potentioValue <= 256){
 			distance = 1; //distance = d
 		}
@@ -70,18 +75,21 @@ void PotentiometerReadingTask(void *p) {
 		else if (potentioValue > 768 && potentioValue <=1023){
 			distance = 4; //distance = 4d
 		}
-		xQueueSendToBack(distanceQueue, &distance, (TickType_t) 5000);
+		xQueueSendToBack(distanceQueue, &distance, (TickType_t) 5000);//send the distance value to the queue for UART printing
 
+		//the following checks if given the desired speed whether the distance is within the safety distance or not
 		if (desiredSpeed == 3 && distance <4){
 			if (currentSpeed == distance){
 				redLedFlag = 1;
 			}
+			//the relationship between current speed and its corresponding safety distance is current speed = distance - 1
 			currentSpeed = distance - 1;
-    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 5000);
+			//send the current speed value to the queue for UART printing
+    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 0);
 		}
 		else if(desiredSpeed == 3 && distance >= 4){
 			currentSpeed = desiredSpeed;
-    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 5000);
+    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 0);
 		}
 
 		if (desiredSpeed == 2 && distance < 3){
@@ -89,11 +97,11 @@ void PotentiometerReadingTask(void *p) {
 				redLedFlag = 1;
 			}
 			currentSpeed = distance - 1;
-    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 5000);
+    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 0);
 		}
 		else if(desiredSpeed == 2 && distance >= 3){
 			currentSpeed = desiredSpeed;
-    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 5000);
+    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 0);
 		}
 
 		if (desiredSpeed == 1 && distance < 2){
@@ -101,17 +109,17 @@ void PotentiometerReadingTask(void *p) {
 				redLedFlag = 1;
 			}
 			currentSpeed = distance - 1;
-    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 5000);
+    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 0);
 		}
 		else if(desiredSpeed == 1 && distance >= 2){
 			currentSpeed = desiredSpeed;
-    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 5000);
+    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 0);
 		}
 		if (desiredSpeed == 0){
 			currentSpeed = desiredSpeed;
-    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 5000);
+    		xQueueSendToBack(currentSpeedQueue, &currentSpeed, (TickType_t) 0);
 		}
-
+        // this gives this task a period of 500ms
 		vTaskDelayUntil(&currentTime, 500);
 	}
 }
@@ -134,6 +142,7 @@ void DisplayInfoTask(void *p){
 			Serial.println(distancePrint);
 		}
 		Serial.println();
+		//this gives the task a period of 500
 		vTaskDelayUntil(&DisplayPeriod, 500);
     }
 }
